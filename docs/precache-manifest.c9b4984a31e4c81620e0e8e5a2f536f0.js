4c8f69c36cb7bbd3c0269480c5c784246a9f5a4e
Emasesosos <j.emmanuel.martinez.e@hotmail.com>
2020-07-09 22:33:02 -0500

    Docs => Github Page

---
+++ docs/precache-manifest.c9b4984a31e4c81620e0e8e5a2f536f0.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8f41fdf17cb073858c7a838df64e198e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ea1e28f5f6ad1b9358ed",
+    "url": "/static/css/main.0a565043.chunk.css"
+  },
+  {
+    "revision": "439b756616db958c0853",
+    "url": "/static/js/2.461d7cdb.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.461d7cdb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ea1e28f5f6ad1b9358ed",
+    "url": "/static/js/main.96143cb7.chunk.js"
+  },
+  {
+    "revision": "daee87f482498cdf724c",
+    "url": "/static/js/runtime-main.29242b1d.js"
+  },
+  {
+    "revision": "f0e512c7ee222713e46e764784c7bc23",
+    "url": "/static/media/logo.f0e512c7.svg"
+  }
+]);
