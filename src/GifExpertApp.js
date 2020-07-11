b5c0a1a54c152eeb458cb50d6390b9bb345bd5fe
Emmanuel <j.emmanuel.martinez.e@hotmail.com>
2020-07-11 00:32:44 -0500

    Prueba Img Producción {'.'+img}

--- src/GifExpertApp.js
+++ src/GifExpertApp.js
@@ -2,8 +2,6 @@ import React, { useState } from 'react';
 import AddCategory from './components/AddCategory';
 import GifGrid from './components/GifGrid';
 import Imagen from './components/Imagen';
-import interestelar from './components/img/Interestelar.jpg';
-import stars from './img/stars.jpg';
 
 const GifExpertApp = () => {
 
@@ -30,27 +28,6 @@ const GifExpertApp = () => {
                     })
                 }
             </ol>
-            <div className="img-prueba">
-                <h3>Imagen Prueba Producción</h3>
-                <div className="images">
-                    <div>
-                        <p>Dinamica = SRC - Components - Img</p>
-                        <img src={'.'+interestelar} alt="interestelar"/>
-                    </div>
-                    <div>
-                        <p>Dinamica = SRC - Img</p>
-                        <img src={'.'+stars} alt="stars"/>
-                    </div>
-                    <div>
-                        <p>SRC - Components - Img</p>
-                        <img src="./components/img/Interestelar.jpg" alt="interestelar"/>
-                    </div>
-                    <div>
-                        <p>SRC - Img</p>
-                        <img src="./img/stars.jpg" alt="stars"/>
-                    </div>
-                </div>
-            </div>
         </div>
     );
 }
