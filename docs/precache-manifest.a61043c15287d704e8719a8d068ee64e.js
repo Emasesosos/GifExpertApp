f4b7bad42c22ff159b0842b6b276d9898459f86c
Emmanuel Martínez <j.emmanuel.martinez.e@hotmail.com>
2020-07-12 10:47:37 -0500

    Docs => Github Page

---
+++ docs/precache-manifest.a61043c15287d704e8719a8d068ee64e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bf6bc035e2572ecf860c99a3f570aa3d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "85b1ca0f8616b780eac3",
+    "url": "/static/css/main.c6a81932.chunk.css"
+  },
+  {
+    "revision": "85a4a86f3fe06f17fcc7",
+    "url": "/static/js/2.8249eaeb.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.8249eaeb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "85b1ca0f8616b780eac3",
+    "url": "/static/js/main.e53de982.chunk.js"
+  },
+  {
+    "revision": "daee87f482498cdf724c",
+    "url": "/static/js/runtime-main.29242b1d.js"
+  }
+]);
